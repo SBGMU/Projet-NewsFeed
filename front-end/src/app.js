1740eb65c95aeaf351722b11cdbf3cc2952507e0
Stephen K <stephenkouanga@gmail.com>
2018-06-15 09:35:08 +0200

    Rectif css website

---
+++ front-end/src/app.js
@@ -0,0 +1,7 @@
+import { messages } from './messages'
+import axios  from './messages'
+
+axios.get('http://localhost')
+.then((response) => console.log(response))
+
+// console.log('App.js Loaded')
---
+++ front-end/src/messages.js
@@ -0,0 +1,12 @@
+const messages = [
+{
+	author: 'test',
+	message :'TextMessage',
+},
+{
+	author: 'test2',
+	message :'TextMessage2',
+}
+]
+
+export {messages}
