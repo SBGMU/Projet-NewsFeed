87a7a619040a3c424f7d8d81d9fc3012a615dbdd
Stephen K <stephenkouanga@gmail.com>
2018-06-19 22:07:24 +0200

    Config folder

--- back-end/router.js
+++ back-end/router.js
@@ -1,15 +1,16 @@
 const express = require('express')
-const router = express.Router()
 const axios = require('axios')
-const news = require('./news.js')
+const router = express.Router()
 
-router.route('/').get(function (request, response) {
-    response.send(news.getHeadLines())
-
-})
 
 router.route('/news').get(function(request, response) {
+
 	axios.get('https://newsapi.org/v2/top-headlines?sources=le-monde&apiKey=055ef3f98c0548b19c3e0e1cf69e1ca4')
-	.then((httpResponse) => response.send(httpResponse.data))
+   .then((httpResponse) => response.send(httpResponse.data))
+
 })
+router.route('/comment').get(function(request, response) {
+
+  })
+
 module.exports = router
--- front-end/src/app.js
+++ front-end/src/app.js
@@ -2,75 +2,23 @@ import axios from 'axios'
 
 const webSocket = new WebSocket('ws://localhost:8888');
 
-webSocket.onmessage = (e) => {
-        let response = JSON.parse(e.data);
-        console.log(e.data)
-        console.log(response)
-        if (response !== undefined) {
-            let newsDivs = ""
-            let i = 0
-            response.reverse().forEach((article) => {
-                if (article.title !== null && article.description !== null && article.urlToImage !== null && article.author !== null) {
-                    if (i === 0) {
-                        newsDivs += "<div class='row'>"
-                    }
-                    newsDivs += "<div class ='col-xl-4 col-sm-6'>"
-                    newsDivs += "<p class='text-center font-weight-bold'>" + article.title + "</p>"
-                    newsDivs += "<img src='" + article.urlToImage + "' class='img-fluid'>"
-                    newsDivs += "<hr><p>" + (article.description.length > 20) ? article.description.slice(0, 20) + " ..." : article.description + "</p>"
-                    newsDivs += "<p>" + article.author + "</p>"
-                    newsDivs += "</div>"
-                    if (i === 0) {
-                        newsDivs += "</div>"
-                    }
-                    i++
-                    if (i === 3) {
-                        i = 0
-                    }
-                }
-            })
-            document.getElementById("news").innerHTML = newsDivs + document.getElementById("news").innerHTML
-            webSocket.send("reset")
-        }
-}
+.then((response) =>{
+
+   console.log(response)    
+
+   var news = "";
+
+   for(var i = 0; i < response.data.articles.length; i++ )
+
+   {
 
 
+       news = news + response.data.articles[i].publishedAt + "  -  "+ response.data.articles[i].title  +"<li href><a href="+response.data.articles[i].url +"><p>"+'<br><img class="" src= '+response.data.articles[i].urlToImage + ' alt="_blank"></img></br>'+"</p></a></li>"
+      
+       console.log(news)
 
-var url = new URL(window.location.href)
+   }
 
-if(url.toString().includes('index.html')){
-    axios.get('http://localhost:8080').then((httpResponse) => {
-        console.log(httpResponse.data)
-        let i = 0;
-
-        httpResponse.data.forEach((article) => {
-            let newsDivs = ""
-            if(article.title !== null && article.description !== null && article.urlToImage !== null && article.author !== null) {
-                if (i === 0) {
-                    newsDivs += "<div class='row'>"
-                }
-                newsDivs += "<div class='col-xl-4 col-sm-6'>"
-                newsDivs += "<p class='text-center font-weight-bold'>" + article.title + "</p>"
-                newsDivs += "<img src='" + article.urlToImage + "' class='img-fluid'>"
-                newsDivs += "<hr><p>" + (article.description.length > 20 ) ? article.description.slice(0,20) + " ..." : article.description + "</p>"
-                newsDivs += "<p>" + article.author + "</p>"
-                newsDivs += "</div>"
-
-                if (i === 0) {
-                    document.getElementById("news").innerHTML += "</div>"
-                }
-                i++
-                if(i === 3){
-                    i = 0
-                }
-                document.getElementById("news").innerHTML = newsDivs + document.getElementById("news").innerHTML
-            }
-        })
-
-    }).catch((e) => {
-        document.getElementById('container').innerHTML = "<h1>Erreur lors de la connexion au Back-End</h1>"
-    })
-
-}else{
-
-}
+   document.getElementById("news").innerHTML = news
+   
+})
