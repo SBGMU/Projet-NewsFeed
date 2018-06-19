59ff1fe4129778ed8de213277e3fc159af00cfa8
Stephen K <stephenkouanga@gmail.com>
2018-06-19 22:54:08 +0200

    Config folder

--- back-end/router.js
+++ back-end/router.js
@@ -1,16 +1,14 @@
 const express = require('express')
-const axios = require('axios')
 const router = express.Router()
-
+const axios = require('axios')
 
 router.route('/news').get(function(request, response) {
-
 	axios.get('https://newsapi.org/v2/top-headlines?sources=le-monde&apiKey=055ef3f98c0548b19c3e0e1cf69e1ca4')
-   .then((httpResponse) => response.send(httpResponse.data))
-
+	.then((httpResponse) => response.send(httpResponse.data))
 })
+
 router.route('/comment').get(function(request, response) {
 
-  })
+})
 
 module.exports = router
